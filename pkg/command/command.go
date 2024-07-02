8ecd721848fc9d41f58f6cbff2d3a909894fe3f3
Tsabit <qaultsabitm@gmail.com>
2024-07-02 22:28:13 +0700

    add more commands

--- pkg/command/command.go
+++ pkg/command/command.go
@@ -3,6 +3,7 @@ package command
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 	"os"
 
 	"github.com/qaultsabit/pokedexcli/pkg/config"
@@ -36,6 +37,21 @@ func GetCommands() map[string]cliCommand {
 			Description: "List pokemon in the location area",
 			Callback:    CommandExplore,
 		},
+		"catch": {
+			Name:        "catch <pokemon_name>",
+			Description: "Attemp to catch a pokemon and add it to your pokedex",
+			Callback:    CommandCatch,
+		},
+		"inspect": {
+			Name:        "inspect <pokemon_name>",
+			Description: "Display information about a caought pokemon",
+			Callback:    CommandInspect,
+		},
+		"pokedex": {
+			Name:        "inspect",
+			Description: "Display all pokemons in your pokedex",
+			Callback:    CommandPokedex,
+		},
 		"exit": {
 			Name:        "exit",
 			Description: "Exit the Pokedex",
@@ -53,7 +69,7 @@ func CommandHelp(cfg *config.Config, args ...string) error {
 	fmt.Println("Welcome to the Pokedex!")
 	commands := GetCommands()
 	for _, command := range commands {
-		fmt.Printf(" - %s: %s\n", command.Name, command.Description)
+		fmt.Printf(" - %s\t: %s\n", command.Name, command.Description)
 	}
 
 	return nil
@@ -100,16 +116,76 @@ func CommandExplore(cfg *config.Config, args ...string) error {
 	if len(args) != 1 {
 		return errors.New("no location area provided")
 	}
-	locationAreaName := args[0]
+	pokemonName := args[0]
 
-	locationArea, err := cfg.PokeapiCleint.GetLocationArea(locationAreaName)
+	locationArea, err := cfg.PokeapiCleint.GetLocationArea(pokemonName)
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf("Pokemons in %s:\n", locationAreaName)
+	fmt.Printf("Pokemons in %s:\n", pokemonName)
 	for _, pokemon := range locationArea.PokemonEncounters {
 		fmt.Println(" _", pokemon.Pokemon.Name)
+	}
+
+	return nil
+}
+
+func CommandCatch(cfg *config.Config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("no pokemon provided")
+	}
+	pokemonName := args[0]
+
+	pokemon, err := cfg.PokeapiCleint.GetPokemon(pokemonName)
+	if err != nil {
+		return err
+	}
+
+	const threshold = 50
+	randNum := rand.Intn(pokemon.BaseExperience)
+	if randNum > threshold {
+		return fmt.Errorf("failed to catch %s", pokemonName)
+	}
+
+	cfg.CaughtPokemon[pokemonName] = pokemon
+
+	fmt.Printf("%s was caught!\n", pokemonName)
+	return nil
+}
+
+func CommandInspect(cfg *config.Config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("no pokemon provided")
+	}
+	pokemonName := args[0]
+
+	pokemon, ok := cfg.CaughtPokemon[pokemonName]
+	if !ok {
+		return errors.New("you haven't caught this pokemon yet")
+	}
+
+	fmt.Printf("Name: %s\n", pokemon.Name)
+	fmt.Printf("Height: %d\n", pokemon.Height)
+	fmt.Printf("Weight: %d\n", pokemon.Weight)
+	fmt.Println("Types:")
+	for _, typ := range pokemon.Types {
+		fmt.Printf(" - %s\n", typ.Type.Name)
+	}
+	fmt.Println("Stats:")
+	for _, stat := range pokemon.Stats {
+		fmt.Printf(" - %s: %d\n", stat.Stat.Name, stat.BaseStat)
+	}
+	return nil
+}
+
+func CommandPokedex(cfg *config.Config, args ...string) error {
+	if len(cfg.CaughtPokemon) == 0 {
+		return errors.New("there are no pokemon in your pokedex")
+	}
+	fmt.Println("Pokemons in your Pokedex:")
+	for _, pokemon := range cfg.CaughtPokemon {
+		fmt.Println(" -", pokemon.Name)
 	}
 
 	return nil
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -10,10 +10,12 @@ type Config struct {
 	PokeapiCleint       pokeapi.Client
 	NextLoactionAreaURL *string
 	PrevLocationAreaURL *string
+	CaughtPokemon       map[string]pokeapi.Pokemon
 }
 
 func NewConfig() Config {
 	return Config{
 		PokeapiCleint: pokeapi.NewClient(time.Minute),
+		CaughtPokemon: make(map[string]pokeapi.Pokemon),
 	}
 }
